675bfc734c07607fce785e2d64bffecd7fcf4deb
SainsshSharma <sainshsharma@gmail.com>
2023-03-03 12:54:58 +0530

    Dashboard

---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,19 @@
+import {Box,useTheme} from '@mui/material'
+import Header from '../../components/Header'
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { tokens } from '../../theme'
+
+const FAQ = ()=>
+{
+    const theme=useTheme()
+    const colors=tokens(theme.palette.mode)
+
+    return <Box m='20px'>
+        <Header title="FAQ" subtitle=""></Header>
+    </Box>
+}
+
+export default FAQ;
